0da3cac11ee625fee129fb21712eb5376958fea8
Kolade Modupe <Kolademodupe007@gmail.com>
2022-06-19 11:30:41 +0100

    commit 1

---
+++ Kolade007/Kolade007/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Kolade007 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kolade007.settings')
+
+application = get_wsgi_application()
